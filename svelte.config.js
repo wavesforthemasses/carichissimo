5a93f1287c374d4eb254260daeef6f8fd4ec0edb
wavesforthemasses <wavesforthemasses@gmail.com>
2024-11-18 17:22:11 +0100

    update

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/' : ''
+			base: process.env.NODE_ENV === 'production' ? '' : ''
 		}
 	}
 };
