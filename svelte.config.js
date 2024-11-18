d0274e37ae8889f8d5bb7cfef60cae02d9a333ff
wavesforthemasses <wavesforthemasses@gmail.com>
2024-11-18 19:26:33 +0100

    update

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,7 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		}),
+		adapter: adapter(),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '' : ''
 		}
