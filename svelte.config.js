706511bbc1ad55ac08eba773b5041750da60daf9
wavesforthemasses <wavesforthemasses@gmail.com>
2024-11-18 19:43:42 +0100

    update

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : '/carichissimo'
 		}
 	}
 };
