8047c172dcb95d4eaf36175346aab4a49a622157
wavesforthemasses <wavesforthemasses@gmail.com>
2024-11-18 19:42:40 +0100

    update

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,11 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
