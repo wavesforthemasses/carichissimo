3a585aeca8cde6238abdac57025ee4499b9c9c7c
wavesforthemasses <wavesforthemasses@gmail.com>
2024-11-18 21:01:12 +0100

    Updates

---
+++ _app/immutable/chunks/paths.C22vTC1B.js
@@ -0,0 +1,1 @@
+var s;const l=((s=globalThis.__sveltekit_16lujcl)==null?void 0:s.base)??"/carichissimo";var a;const e=((a=globalThis.__sveltekit_16lujcl)==null?void 0:a.assets)??l;export{e as a,l as b};
---
+++ _app/immutable/entry/start.CrySxtra.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Dvni0mWs.js";export{t as start};
