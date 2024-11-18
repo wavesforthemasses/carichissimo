3682f6d7624100ff2b29dee377ff3db6f7344a55
wavesforthemasses <wavesforthemasses@gmail.com>
2024-11-18 20:01:38 +0100

    update

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,9 @@ const config = {
 		adapter: adapter({
 			assets: 'build',
 			fallback: '404.html',
-			pages: 'build'
+			pages: 'build',
+			precompress: false,
+			strict: true
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
